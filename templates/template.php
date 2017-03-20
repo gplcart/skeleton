9a367bb6640c6e665c84589baf63d3697223faef
GplCart <gplcart.software@gmail.com>
2017-03-20 23:07:19 +0200

    Update template.php

--- templates/template.php
+++ templates/template.php
@@ -7,34 +7,36 @@
 */
 ?>
 <form method="post" class="form-horizontal">
-  <input type="hidden" name="token" value="<?php //echo $this->prop('token');// Required, uncomment!!  ?>">
+  <input type="hidden" name="token" value="<?php echo '<?php echo $this->prop("token"); ?>'; ?>">
   <div class="panel panel-default">
     <div class="panel-body">
       <div class="form-group">
-        <label class="col-md-2 control-label">Status</label>
+        <label class="col-md-2 control-label"><?php echo '<?php echo $this->text("Name"); ?>'; ?></label>
         <div class="col-md-6">
-          <div class="btn-group" data-toggle="buttons">
-            <label class="btn btn-default<?php echo empty($settings['status']) ? '' : ' active'; ?>">
-              <input name="settings[status]" type="radio" autocomplete="off" value="1"<?php echo empty($settings['status']) ? '' : ' checked'; ?>>
-              Enabled
+          <input name="settings[name]" class="form-control" placeholder="Placeholder" value="<?php echo '<?php echo isset($settings["name"]) ? $settings["name"] : ""; ?>'; ?>">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-md-2 control-label"><?php echo '<?php echo $this->text("File"); ?>'; ?></label>
+        <div class="col-md-6">
+          <input type="file">
+          <div class="help-block">Example block-level help text here.</div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-md-6 col-md-offset-2">
+          <div class="checkbox">
+            <label>
+              <input name="settings[status]" type="checkbox"<?php echo '<?php echo empty($settings["status"]) ? "" : " checked"; ?>'; ?>> Check me out
             </label>
-            <label class="btn btn-default<?php echo empty($settings['status']) ? ' active' : ''; ?>">
-              <input name="settings[status]" type="radio" autocomplete="off" value="0"<?php echo empty($settings['status']) ? ' checked' : ''; ?>>
-              Disabled
-            </label>
-          </div>
-          <div class="help-block">
-            <?php //echo $this->text('Status'); //Wrap translatable strings in $this->text() ?>
           </div>
         </div>
       </div>
       <div class="form-group">
         <div class="col-md-4 col-md-offset-2">
           <div class="btn-toolbar">
-            <a href="<?php //echo $this->url('admin/module/list');  ?>" class="btn btn-default"><i class="fa fa-reply"></i> Cancel</a>
-            <button class="btn btn-default save" name="save" value="1">
-              <i class="fa fa-floppy-o"></i> Save
-            </button>
+            <a href="<?php echo '<?php echo $this->url("admin/module/list"); ?>'; ?>" class="btn btn-default"><?php echo '<?php echo $this->text("Cancel"); ?>'; ?></a>
+            <button class="btn btn-default save" name="save" value="1"><?php echo '<?php echo $this->text("Save"); ?>'; ?></button>
           </div>
         </div>
       </div>

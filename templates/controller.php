c6266000b0c16e38fdbc7bcba6543fedec789fc1
GplCart <gplcart.software@gmail.com>
2017-04-12 14:37:39 +0300

    Remove skeleton author from PHPDoc in templates

--- templates/controller.php
+++ templates/controller.php
@@ -1,7 +1,6 @@
 /**
  * @package <?php echo $module['name']; ?> 
  * @author <?php echo $module['author']; ?> 
- * @author Skeleton https://github.com/gplcart/skeleton 
  * @copyright Copyright (c) <?php echo date('Y'); ?>, <?php echo $module['author']; ?> 
  * @license <?php echo $module['license_url']; ?> 
  */

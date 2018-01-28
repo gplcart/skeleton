b7570dd67336c30d08657262c5017cb33cc644cb
GplCart <gplcart.software@gmail.com>
2018-01-28 18:08:17 +0200

    Add code template

---
+++ templates/code/config.php
@@ -0,0 +1,15 @@
+/**
+ * @package <?php echo $module['name']; ?> 
+ * @author <?php echo $module['author']; ?> 
+ * @copyright Copyright (c) <?php echo date('Y'); ?>, <?php echo $module['author']; ?> 
+ * @license <?php echo $module['license_url']; ?> 
+ */
+
+/**
+ * @todo Format the source code
+ */
+return array(
+  // Array of key => value configuration options. To get the array just require the file:
+  // $config = require 'full/path/to/file';
+  // OR $config = gplcart_config_get('full/path/to/file');
+);

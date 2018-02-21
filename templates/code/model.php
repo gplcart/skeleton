06cf34583c8c5353c2205f2bd0a4301c751c7ad5
GplCart <gplcart.software@gmail.com>
2018-02-21 15:22:13 +0200

    Fix code template

--- templates/code/model.php
+++ templates/code/model.php
@@ -1,17 +1,17 @@
 /**
- * @package <?php echo $module['name']; ?>
- * @author <?php echo $module['author']; ?>
- * @copyright Copyright (c) <?php echo date('Y'); ?>, <?php echo $module['author']; ?>
- * @license <?php echo $module['license_url']; ?>
+ * @package <?php echo $module['name']; ?> 
+ * @author <?php echo $module['author']; ?> 
+ * @copyright Copyright (c) <?php echo date('Y'); ?>, <?php echo $module['author']; ?> 
+ * @license <?php echo $module['license_url']; ?> 
  */
 
-namespace gplcart\modules\<?php echo $module['id']; ?>\models;
+namespace gplcart\modules\<?php echo $module['id']; ?>\models; 
 
 /**
  * Manages basic behaviors and data related to <?php echo $module['name']; ?> module
  * @todo Format the source code
  */
-class <?php echo $module['class_name']; ?>
+class <?php echo $module['class_name']; ?> 
 {
     /**
      * Constructor
